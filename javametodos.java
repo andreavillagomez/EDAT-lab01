5de806b7dbe7637280a90950c4016c41c8aaf1af
eddym <eddy@curl.cl>
2022-05-14 21:06:07 -0500

    explique como se declara un metodo dentro de una clase

---
+++ javametodos.java
@@ -0,0 +1,10 @@
+public  class javametodos {
+    public static void main(String[] args) {
+        
+    }
+    static void hola(){
+        //la palabra static significa que el metodo pertenece al main
+        //void significa que no tiene un valor de retorno
+        //hola es el nombre del metodo
+    }
+}
