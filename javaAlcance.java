218e64e085a2427a607661d884d761dfdcb9d372
eddym <eddy@curl.cl>
2022-05-14 20:05:18 -0500

    solo se puede acceder a las variables dentro de la region en la que fueron creadas

---
+++ javaAlcance.java
@@ -0,0 +1,7 @@
+public class javaAlcance {
+    public static void main(String[] args) {
+        int entero=10;
+        //solo podemos acceder a las variable dentro de la region en la que fueron creadas 
+        System.out.println("el entro es "+entero);
+    }
+}
