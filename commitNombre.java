63520f9a605334effe496266604f74a510452fed
eddym <eddy@curl.cl>
2022-05-16 17:27:40 -0500

    agregue el saludo al ingresar un nombre

--- commitNombre.java
+++ commitNombre.java
@@ -1,6 +1,13 @@
+import java.util.*;
 public class  commitNombre {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
         System.out.println("BIENVENIDO AL CURSO DE EDA ");
+        
+        Scanner sc = new Scanner (System.in);
+        System.out.println("ingresa tu nombre");
+        String nombre=sc.nextLine();
+
+        System.out.println("HOLA " +nombre);
     }
 }
