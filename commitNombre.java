c9ebb243added7f403906308457ee60598e83b8e
eddym <eddy@curl.cl>
2022-05-16 17:24:52 -0500

    agregue la bienvenida al curso

---
+++ commitNombre.java
@@ -0,0 +1,6 @@
+public class  commitNombre {
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+        System.out.println("BIENVENIDO AL CURSO DE EDA ");
+    }
+}
